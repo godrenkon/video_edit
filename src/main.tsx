1a98205349322f965f8450ff9bc898432749b405
godrenkon <renkomkun@gmail.com>
2026-09-16 02:04:07 +0900

    style: load recovery dialog styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './layout-fixes.css';
+import './recovery.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
