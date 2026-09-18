5a795102270c3247aa3aaac76dbd6f1f194b59c7
godrenkon <renkomkun@gmail.com>
2026-09-18 21:34:49 +0900

    feat: show realtime peak RMS meter in preview toolbar

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -21,6 +21,7 @@ import {
 } from '../render/syntheticLayers';
 import type { AssetMeta, Clip, Project } from '../types/editor';
 import { PlaybackDiagnostics } from './PlaybackDiagnostics';
+import { PreviewAudioMeter } from './PreviewAudioMeter';
 import '../preview-synthetic.css';
 
 interface Props {
@@ -302,6 +303,7 @@ export function Preview({ project, time, playing, onTogglePlay, onTime }: Props)
         <div className="previewToolbar">
           <span>{project.width}×{project.height}</span>
           <span>{project.fps} fps</span>
+          <PreviewAudioMeter playing={playing} />
           <PlaybackDiagnostics time={time} playing={playing} fps={project.fps} />
           <button className="miniBtn" type="button" onClick={toggleFullscreen} title={fullscreen ? 'フルスクリーンを終了' : 'フルスクリーン'} aria-label={fullscreen ? 'フルスクリーンを終了' : 'フルスクリーン'}>
             {fullscreen ? <Minimize2 size={14} /> : <Maximize2 size={14} />}
