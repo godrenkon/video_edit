5cce9ffe0b2b7c6b6f393b494392b1d5fb0a60e9
godrenkon <renkomkun@gmail.com>
2026-09-19 22:00:18 +0900

    feat: add dip-to-black transition kind

--- src/types/editor.ts
+++ src/types/editor.ts
@@ -15,7 +15,7 @@ export type BlendMode =
   | 'add';
 
 export type EffectParameterValue = number | string | boolean | number[];
-export type TransitionKind = 'dissolve' | 'slide-left' | 'slide-right' | 'slide-up' | 'slide-down' | 'wipe-left' | 'wipe-right' | 'wipe-up' | 'wipe-down';
+export type TransitionKind = 'dissolve' | 'dip-black' | 'slide-left' | 'slide-right' | 'slide-up' | 'slide-down' | 'wipe-left' | 'wipe-right' | 'wipe-up' | 'wipe-down';
 
 export interface ClipTransition {
   kind: TransitionKind;
