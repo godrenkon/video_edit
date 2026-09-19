bd460c22aeacdfa8aadcf8e24605701875a546e1
godrenkon <renkomkun@gmail.com>
2026-09-19 22:25:40 +0900

    feat: route tonal range controls through pixel compositor

--- src/render/effectEvaluation.ts
+++ src/render/effectEvaluation.ts
@@ -11,7 +11,7 @@ const CANVAS_FILTER_EFFECTS = new Set([
   'drop-shadow',
 ]);
 const VISUAL_OVERLAY_EFFECTS = new Set(['vignette', 'temperature-tint']);
-const PIXEL_EFFECTS = new Set(['sharpen', 'chroma-key', 'levels', 'lift-gamma-gain']);
+const PIXEL_EFFECTS = new Set(['sharpen', 'chroma-key', 'levels', 'lift-gamma-gain', 'tonal-ranges']);
 
 export interface ResolvedVignette {
   amount: number;
