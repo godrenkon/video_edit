91485f452ed3a1f47cda3c4d1b9262d313f53e03
godrenkon <renkomkun@gmail.com>
2026-09-19 22:24:38 +0900

    feat: route lift gamma gain through pixel compositor

--- src/render/effectEvaluation.ts
+++ src/render/effectEvaluation.ts
@@ -11,7 +11,7 @@ const CANVAS_FILTER_EFFECTS = new Set([
   'drop-shadow',
 ]);
 const VISUAL_OVERLAY_EFFECTS = new Set(['vignette', 'temperature-tint']);
-const PIXEL_EFFECTS = new Set(['sharpen', 'chroma-key', 'levels']);
+const PIXEL_EFFECTS = new Set(['sharpen', 'chroma-key', 'levels', 'lift-gamma-gain']);
 
 export interface ResolvedVignette {
   amount: number;
